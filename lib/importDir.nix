6385004fa8bfa6b22b2d01da8f2b90c9143ebae3
zimbatm <zimbatm@zimbatm.com>
2019-03-16 14:53:36 +0100

    introduce importDir
    avoid repeating myself
    
    to create a new image, create a new folder under ./images that contains
    a default.nix. That's it.

---
+++ lib/importDir.nix
@@ -0,0 +1,21 @@
+{ lib }:
+
+importFn: baseDir:
+let
+  dirEntries =
+    builtins.attrNames
+      (lib.filterAttrs
+        (k: v: v == "directory")
+        (builtins.readDir baseDir));
+
+  absDirs =
+    builtins.map
+      (dir: "${toString baseDir}/${dir}")
+      dirEntries;
+
+  imports =
+    builtins.map
+      (dir: { name = builtins.baseNameOf dir; value = importFn dir; })
+      absDirs;
+in
+  builtins.listToAttrs imports
--- overlay.nix
+++ overlay.nix
@@ -1,18 +1,11 @@
-_: pkgs: {
-  # lib stuff can be in the top-level
+_: pkgs: let
+  importDir = import ./lib/importDir.nix {
+    inherit (pkgs) lib; 
+  };
+in {
+  # builder stuff can be in the top-level
   buildCLIImage = pkgs.callPackage ./lib/buildCLIImage.nix {};
 
   # docker images must be lower-cased
-  docker-nixpkgs = {
-
-    bash = pkgs.callPackage ./images/bash {};
-    busybox = pkgs.callPackage ./images/busybox {};
-    curl = pkgs.callPackage ./images/curl {};
-    docker-compose = pkgs.callPackage ./images/docker-compose {};
-    kubectl = pkgs.callPackage ./images/kubectl {};
-    kubernetes-helm = pkgs.callPackage ./images/kubernetes-helm {};
-    nix = pkgs.callPackage ./images/nix {};
-    nix-unstable = pkgs.callPackage ./images/nix-unstable {};
-
-  };
+  docker-nixpkgs = importDir (path: pkgs.callPackage path {}) ./images;
 }
