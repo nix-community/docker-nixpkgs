9258d92617713549c6b8f40515007d7d961056a4
Antoine Eiche <lewo@abesis.fr>
2019-04-25 20:44:33 +0200

    Use niv to fetch the nix-container-images repository
    This allows us to easily update it by running `niv update`.

--- default.nix
+++ default.nix
@@ -1,9 +1,7 @@
 { nixpkgs ? <nixpkgs> }:
 let
-  nix-container-images = builtins.fetchTarball {
-    url = "https://github.com/nlewo/nix-container-images/archive/7577da87f7249442b51359e36a0f0493949a14d6.tar.gz";
-    sha256 = "0cv3b8gngvb2my1rqs2kxszmwb3s3i72j1wp6jin5f1wjp9km32w";
-  };
+  sources = import ./nix/sources.nix;
+  nix-container-images = sources."nix-container-images";
 in
 import nixpkgs {
   # docker images run on Linux
