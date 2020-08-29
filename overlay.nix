0dd13bc391602d8d30daa6908a8348f2aff83833
Silvan Mosberger <contact@infinisil.com>
2020-08-29 03:13:56 +0200

    Minor cleanup

--- overlay.nix
+++ overlay.nix
@@ -1,16 +1,9 @@
-_: pkgs: let
-  importDir = import ./lib/importDir.nix {
-    inherit (pkgs) lib;
-  };
-in {
+_: pkgs: {
 
   scripts = pkgs.callPackage ./scripts {};
 
   # builder stuff can be in the top-level
   buildCLIImage = pkgs.callPackage ./lib/buildCLIImage.nix {};
-
-  # docker images must be lower-cased
-  docker-nixpkgs = importDir (path: pkgs.callPackage path {}) ./images;
 
   # used to build nix-env compatible user environments
   mkUserEnvironment = pkgs.callPackage ./lib/mkUserEnvironment.nix {};
