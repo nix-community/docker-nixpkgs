d454de5f077876eeb0255854eda089d45b98ab57
zimbatm <zimbatm@zimbatm.com>
2020-11-27 15:32:49 +0100

    ci: add nixos-20.09

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = builtins.fetchTarball "channel:nixos-20.03";
+  nixpkgs = builtins.fetchTarball "channel:nixos-20.09";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 with pkgs;
