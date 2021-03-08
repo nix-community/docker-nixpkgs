887722a6037f8a3d574765970da122c7a617f449
zimbatm <zimbatm@zimbatm.com>
2021-03-08 12:17:23 +0100

    nix-flakes: enable flakes in the config
    Fixes #18

--- images/nix-flakes/default.nix
+++ images/nix-flakes/default.nix
@@ -1,6 +1,16 @@
 { docker-nixpkgs
 , nixFlakes
+, writeTextFile
 }:
 docker-nixpkgs.nix.override {
   nix = nixFlakes;
+  extraContents = [
+    (writeTextFile {
+      name = "nix.conf";
+      destination = "/etc/nix/nix.conf";
+      text = ''
+        experimental-features = nix-command flakes
+      '';
+    })
+  ];
 }
