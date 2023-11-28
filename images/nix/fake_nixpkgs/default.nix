bff806291e0e282580115089136edcbe87428fdc
Narek Asadorian <narek.asadorian@gmail.com>
2023-11-27 19:25:07 -0800

    Fix broken link in error message

--- images/nix/fake_nixpkgs/default.nix
+++ images/nix/fake_nixpkgs/default.nix
@@ -3,7 +3,7 @@ throw ''
   This container doesn't include nixpkgs.
 
   The best way to work around that is to pin your dependencies. See
-    https://nix.dev/tutorials/towards-reproducibility-pinning-nixpkgs.html
+    https://nix.dev/tutorials/first-steps/towards-reproducibility-pinning-nixpkgs.html
 
   Or if you must, override the NIX_PATH environment variable with eg:
     "NIX_PATH=nixpkgs=channel:nixos-unstable"
