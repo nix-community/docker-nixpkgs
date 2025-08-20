e5166d7389a0ce61b397334c57c7e1cc75b1f945
MiggL <miguel.muller@tutanota.com>
2025-08-20 22:45:59 +0200

    Update nix version in nix-unstable-static to 2.30.3
    Fixes #92

--- images/nix-unstable-static/default.nix
+++ images/nix-unstable-static/default.nix
@@ -19,8 +19,8 @@ let
 
   # Get nix from Hydra because the nixpkgs one is not fully static
   nixStaticBin = fetchurl {
-    url = "https://hydra.nixos.org/build/181573550/download/1/nix";
-    hash = "sha256-zO2xJhQIrLtL/ReTlcorjwsaTO1W5Rnr+sXwcLcujok=";
+    url = "https://hydra.nixos.org/build/305222051/download/1/nix";
+    hash = "sha256-OahnvQ/OKnRhbXaIJ7iEQYu86ECGtUqwW8XrryVkXaM=";
   };
 
   nixSymlinks = [
